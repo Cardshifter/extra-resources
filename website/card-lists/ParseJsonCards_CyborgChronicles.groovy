948ef6681d6b918891bd005b619aabc53c78dcc7
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-07-07 17:16:07 -0400

    Update ParseJsonCards_CyborgChronicles.groovy

--- website/card-lists/ParseJsonCards_CyborgChronicles.groovy
+++ website/card-lists/ParseJsonCards_CyborgChronicles.groovy
@@ -26,7 +26,7 @@ def endMechQuoteStyle = '</code>'
 
 // Bio cards
 println '<!-- BIO CARDS -->\n'
-println '<!-- Add the resulting HTML to /io.web/src/main/webapp/WEB-INF/cyborg-chronicles/cards-bio.html' -->\n'
+println '<!-- Add the resulting HTML to /io.web/src/main/webapp/WEB-INF/cyborg-chronicles/cards-bio.html -->\n'
 json.each {
     if (it."type" == "Bio") {
         println "<${cardHeader}>${it."name"} ${it."ATTACK"} / ${it."HEALTH"}</${cardHeader}>"
@@ -60,7 +60,7 @@ json.each {
 
 // Mech cards
 println '<!-- MECH CARDS -->\n'
-println '<!-- Add the resulting HTML to /io.web/src/main/webapp/WEB-INF/cyborg-chronicles/cards-mech.html' -->\n'
+println '<!-- Add the resulting HTML to /io.web/src/main/webapp/WEB-INF/cyborg-chronicles/cards-mech.html -->\n'
 json.each {
     if (it."type" == "Mech") {
         println "<${mechCardHeader}>${it."name"} ${it."ATTACK"} / ${it."HEALTH"}</${endMechCardHeader}>"
@@ -95,7 +95,7 @@ json.each {
 
 // Other cards
 println '<!-- OTHER CARDS -->\n'
-println '<!-- Add the resulting HTML to /io.web/src/main/webapp/WEB-INF/cyborg-chronicles/cards-cybernetic.html' -->\n'
+println '<!-- Add the resulting HTML to /io.web/src/main/webapp/WEB-INF/cyborg-chronicles/cards-cybernetic.html -->\n'
 json.each {
     if (it."type" != "Bio" && it."type" != "Mech") {
         println "<${cardHeader}>${it."name"}</${cardHeader}>"
